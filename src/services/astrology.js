496c25bbb68ead273d647c8375b3e4c7e4acf0ac
miticous <murilo.medeiros.gyn@gmail.com>
2021-11-23 05:39:42 -0300

    setup/⚙️ adding typescript and making runnable

--- src/services/astrology.js
+++ src/services/astrology.js
@@ -5,36 +5,33 @@ const api = axios.create({
   maxContentLength: 13290129312
 });
 
-export const getAstral = async ({ name, latitude, longitude, birthdate, birthplaceFuso }) => {
-  try {
-    const { data } = await api.post('/indices', {
-      name,
-      latitude,
-      longitude,
-      birthdate,
-      birthplace_fuso: birthplaceFuso,
-      horaverao: 'false',
-      placename: ''
-    });
+export const getAstral = async ({ name, latitude, longitude, birthdate, birthplaceFuso }) => ({
+  chart_id: '123123',
+  zodiac: 'pisces',
+  instinto: 'pisces',
+  mandala:
+    'https://casadaarte.vteximg.com.br/arquivos/ids/211964/Mandala-MA3517-3-.jpg?v=636322741987070000'
+});
 
-    console.log('Success on using astrology api INDICES');
+// console.log('Success on using astrology api INDICES');
 
-    return data;
-  } catch (error) {
-    console.log(`Error on using astrology api INDICES ${error?.message}`);
-    throw new Error(error.message);
-  }
-};
+// return data;
 
 export const getTexts = async ({ chartId }) => {
   try {
-    const { data } = await api.get(`/textos/${chartId}`);
+    // const { data } = await api.get(`/textos/${chartId}`);
 
     console.log('Success on using astrology api');
 
     return {
-      chartId: data?._id,
-      texts: data?.texts
+      chartId: '123123',
+      texts: [
+        {
+          text: 'Testando esse texto',
+          title: 'Instinto',
+          subtitle: 'Testaaaaa'
+        }
+      ]
     };
   } catch (error) {
     console.log(`Error on using astrology api ${error?.message}`);
